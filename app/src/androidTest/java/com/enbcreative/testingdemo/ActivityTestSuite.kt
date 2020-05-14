e189be3762f9af95b9212f7d27df64cae99752b7
Ercan Duman <ercanduman30@gmail.com>
2020-05-14 08:55:56 +0300

    Test suite created (ActivityTestSuite.kt) in order to running all test at once

---
+++ app/src/androidTest/java/com/enbcreative/testingdemo/ActivityTestSuite.kt
@@ -0,0 +1,8 @@
+package com.enbcreative.testingdemo
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(MainActivityTest::class, SecondActivityTest::class)
+class ActivityTestSuite
