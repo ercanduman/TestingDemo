a82dbbcae8d84587509c861d8b72fd41e120f194
Ercan Duman <ercanduman30@gmail.com>
2020-05-15 11:33:51 +0300

    Testing material dialog and capturing input data cases added

--- app/src/androidTest/java/com/enbcreative/testingdemo/SecondActivityTest.kt
+++ app/src/androidTest/java/com/enbcreative/testingdemo/SecondActivityTest.kt
@@ -2,6 +2,8 @@ package com.enbcreative.testingdemo
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -52,5 +54,39 @@ class SecondActivityTest {
         onView(withId(R.id.parent_main)).check(matches(isDisplayed()))
         onView(withId(R.id.btn_next_main)).perform(click())
         onView(withId(R.id.parent_second)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_launch_dialog_and_check_if_dialog_not_dismissed_after_ok_btn_click() {
+        onView(withId(R.id.btn_launch_dialog_second_activity)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_launch_dialog_second_activity)).perform(click())
+
+        // check if any member of dialog is displayed
+        onView(withText(R.string.dialog_title)).check(matches(isDisplayed()))
+
+        // then click ok button of dialog and check if dialog not dismissed
+        onView(withText(android.R.string.ok)).perform(click()) // android.R.string.ok is the text we set to ok button
+        onView(withText(R.string.dialog_title)).check(matches(isDisplayed())) // dialog should still be displayed
+    }
+
+    @Test
+    fun test_launch_dialog_and_capture_input_data() {
+        onView(withId(R.id.btn_launch_dialog_second_activity)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_launch_dialog_second_activity)).perform(click())
+
+        // check if dialog is displayed
+        onView(withId(R.id.md_input_message)).check(matches(isDisplayed())) // md_input_message is id of input layout in material dialog
+
+        val expectedName = "Ercan"
+        onView(withId(R.id.md_input_message)).perform(typeText(expectedName)) // writes name to input layout
+
+        /** click on "ok" button and check if input text written to title_second */
+        onView(withText(android.R.string.ok)).perform(click())
+
+        // make sure dialog is gone
+        onView(withId(R.id.md_input_message)).check(doesNotExist())
+
+        // check if text set to layout
+        onView(withId(R.id.title_second)).check(matches(withText(expectedName)))
     }
 }
--- app/src/main/java/com/enbcreative/testingdemo/SecondActivity.kt
+++ app/src/main/java/com/enbcreative/testingdemo/SecondActivity.kt
@@ -2,7 +2,10 @@ package com.enbcreative.testingdemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,6 +13,28 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        btn_previous_second_activity.setOnClickListener {startActivity( Intent(this, MainActivity::class.java))}
+        btn_previous_second_activity.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        btn_launch_dialog_second_activity.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        MaterialDialog(this).show {
+            input(
+                waitForPositiveButton = true,
+                allowEmpty = false,
+                hint = "Input message here..."
+            ) { _, inputData ->
+                handleInputData(inputData.toString())
+            }
+            title(R.string.dialog_title)
+            positiveButton(android.R.string.ok)
+        }
+    }
+
+    private fun handleInputData(inputData: String) {
+        Log.d(this.javaClass.simpleName, "Dialog input data: $inputData")
+        title_second.text = inputData
     }
 }
